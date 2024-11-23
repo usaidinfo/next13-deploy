31bd60f0d3fb039a93ab4a39a3d97a627cbcfbbe
usaid mustafa <164148478+usaidinfo@users.noreply.github.com>
2024-11-23 22:53:55 +0530

    Update and rename next.config.ts to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-  webpack(config: { resolve: { alias: { [x: string]: any; }; }; }) {
+  webpack(config) {
     // Add the alias to Webpack
     config.resolve.alias['@components'] = path.join(__dirname, 'src/components');
     return config;
